c3d1c302cc6c7b1dee2cb73569ad35065ad66bdb
edwin9905 <qkr990509@naver.com>
2023-05-02 23:27:29 +0900

    KK

--- src/ft_free.c
+++ src/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: gkwon <gkwon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 21:58:35 by edwin             #+#    #+#             */
-/*   Updated: 2023/05/02 22:42:35 by gkwon            ###   ########.fr       */
+/*   Updated: 2023/05/02 23:25:59 by gkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ void	ft_free_command(t_command **cmd, t_sys_info *info)
 	while (i < info->cmd_cnt)
 	{
 		j = 0;
-		if (cmd[i]->program[j])
-			while (cmd[i]->program[j])
-				free(cmd[i]->program[j++]);
-		free(cmd[i]->program);
+		if ((*cmd + i)->program[j])
+			while ((*cmd + i)->program[j])
+				free((*cmd + i)->program[j++]);
+		free((*cmd + i)->program);
 		j = -1;
 		while (++j < 4)
-			if (cmd[i]->info[j])
-				free(cmd[i]->info[j]);
-		free(cmd[i]->info);
+			if ((*cmd + i)->info[j])
+				free((*cmd + i)->info[j]);
+		free((*cmd + i)->info);
 		i++;
 	}
 	free(*cmd);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gkwon <gkwon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 18:00:27 by gkwon             #+#    #+#             */
-/*   Updated: 2023/05/02 22:56:13 by gkwon            ###   ########.fr       */
+/*   Updated: 2023/05/02 23:26:12 by gkwon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,19 @@ int	tokenize(char *line, t_command **cmd, t_sys_info *info)
 		return (0);
 	}
 	while (++i < info->cmd_cnt)
-		init_cmd(nodes[i], cmd[i]);
+		init_cmd(nodes[i], *cmd + i);
 	builtin_check(cmd, info);
 	i = -1;
 	while (++i < info->cmd_cnt)
 	{
 		j = -1;
-		while (cmd[i]->program[++j])
-			printf("program is : %s\n", cmd[i]->program[j]);
-		printf("is builtin : %d\n", cmd[i]->built_in);
-		printf("input is : %s\n", cmd[i]->info[0]);
-		printf("output is : %s\n", cmd[i]->info[1]);
-		printf("del is : %s\n", cmd[i]->info[2]);
-		printf("output_append is : %s\n", cmd[i]->info[3]);
+		while ((*cmd + i)->program[++j])
+			printf("program is : %s\n", (*cmd + i)->program[j]);
+		printf("is builtin : %d\n", (*cmd + i)->built_in);
+		printf("input is : %s\n", (*cmd + i)->info[0]);
+		printf("output is : %s\n", (*cmd + i)->info[1]);
+		printf("del is : %s\n", (*cmd + i)->info[2]);
+		printf("output_append is : %s\n", (*cmd + i)->info[3]);
 	}
 	free(nodes);
 	return (0);
@@ -80,11 +80,41 @@ int	display(t_sys_info *info, char **envp)
 					cmd[i].output_append = cmd[i].info[3];
 			}
 			_jungyeok(cmd, envp, info->cmd_cnt - 1);
+			(void)envp;
 			ft_free_command(&cmd, info);
 		}
 	}
 	return (0);
 }
+
+//int	display(t_sys_info *info, char **envp)
+//{
+//	t_command	**cmd;
+//	char		*line;
+//	int			i;
+
+//	set_signal_handlers();
+//	while (1)
+//	{
+//		line = readline("bash-3.3$ ");
+//		if (ft_strncmp(line, "\0", 1))
+//		{
+//			if (!ft_strncmp(line, "exit", 4))
+//				ft_exit(0);
+//			info->cmd_cnt = pipe_cnt(line) + 1;
+//			cmd = ft_calloc(sizeof(t_command *), info->cmd_cnt);
+//			i = 0;
+//			while (i < info->cmd_cnt)
+//				cmd[i++] = ft_calloc(sizeof(t_command), 1);
+//			tokenize(line, cmd, info);
+//			add_history(line);
+//			(void) envp;
+//			//_jungyeok(&command, envp);
+//			ft_free_command(cmd, info);
+//		}
+//	}
+//	return (0);
+//}
 
 int	main(int ac, char **av, char **envp)
 {
